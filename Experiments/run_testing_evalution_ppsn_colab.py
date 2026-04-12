e7c10cc7414983a693dbeb9f0720464fe2c5ed0e
dan-gn <78847525+dan-gn@users.noreply.github.com>
2026-04-12 20:36:30 +0100

    Update run_testing_evalution_ppsn_colab.py

--- Experiments/run_testing_evalution_ppsn_colab.py
+++ Experiments/run_testing_evalution_ppsn_colab.py
@@ -1,5 +1,10 @@
-from experiments10_ppsn_gcolab import objective_function, set_model_and_environment_parameters
+""" 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Import required libraries and functions
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
 
+from experiments10_ppsn_gcolab import objective_function, set_model_and_environment_parameters
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -12,19 +17,40 @@ import random
 
 from concurrent.futures import ProcessPoolExecutor
 
+
+""" 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ARGUMENTS
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
+
 SEED = 1996
-eval_tries = 1000
+eval_tries = 2
 CORES = 7
 
 MODELS = []
 MODELS.append('abcd')
 MODELS.append('neuromodulated_hb')
 MODELS.append('static')
+MODELS.append('static_double')
 
 ENVIRONMENTS = []
 ENVIRONMENTS.append('MountainCar-v0')
 ENVIRONMENTS.append('CartPole-v1')
 ENVIRONMENTS.append('Acrobot-v1')
+
+# Read the results dataframe
+# log_folder = f'../drive/MyDrive/PPSN26/Experiments/Results/test_ppsn_march/'
+log_folder = f'Experiments/Results/test_ppsn_march/'
+
+new_file = log_folder + f'ppsn_testing_results_testingggg.csv'
+
+
+""" 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+SECONDARY FUNCTIONS
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
 
 def set_seed(seed):
     if SEED is not None:
@@ -35,32 +61,61 @@ def set_seed(seed):
         random.seed(SEED)
 
 
-# Read the results dataframe
-log_folder = f'../drive/MyDrive/PPSN26/Experiments/Results/test_ppsn_march/'
-df = pd.concat([pd.read_csv(f'{log_folder}/experiments_log_colab_exp{colab_file}.csv') for colab_file in range(1, 4)], ignore_index=True)
+def filter_experiments(df):
+    # Simple filters
+    df = df[df["model"].isin(MODELS)]
+    df = df[df["environment"].isin(ENVIRONMENTS)]
+    df = df[df["cores"] == 47]
+    df = df[df["population_size"] == 100]
+    hidden_sizes = ['[64, 32]', '[150, 75]']
+    df = df[df["hidden_size"].isin(hidden_sizes)]
 
-new_file = log_folder + f'ppsn_testing_results_it_fix.csv'
+    # Filter by number of iterations per each environment
+    acrobot_parameters = {
+        'environment' : 'Acrobot-v1',
+        'max_iterations' : 100,
+    }
+    cartpole_parameters = {
+        'environment' : 'CartPole-v1',
+        'max_iterations' : 100,
+    }
+    mountaincar_parameters = {
+        'environment' : 'MountainCar-v0',
+        'max_iterations' : 500,
+    }
+    parameters = [acrobot_parameters, cartpole_parameters, mountaincar_parameters]
+    filtered_dfs = [
+        df[
+            (df['environment'] == params['environment']) &
+            (df['max_iterations'] == params['max_iterations'])
+        ]
+        for params in parameters
+    ]
+    df = pd.concat(filtered_dfs)
+
+    parameters = [acrobot_parameters, cartpole_parameters, mountaincar_parameters]
+    filtered_dfs = [
+        df[
+            (df['environment'] == params['environment']) &
+            (df['max_iterations'] == params['max_iterations'])
+        ]
+        for params in parameters
+    ]
+    df = pd.concat(filtered_dfs)
 
 
-# Filter by environment
-# environment = ''
-# df_filtered = df[df["environment"] == environment]
 
-# Filter by model
-# df_filtered = df_filtered[df_filtered["model"] == "static"]
-# df_filtered.loc[df_filtered["model"] == "abcd", "lambda_exp"] = -1
-# df_filtered.loc[df_filtered["model"] == "static", "lambda_exp"] = -2
+    # Remove duplicates
+    cols = ['model', 'environment', 'lambda_exp', 'seed']
+    df = df[~df.duplicated(subset=cols, keep='last')]
 
-df['testing'] = None
+    # Sort the dataframe
+    df = df.sort_values(by=cols)
 
-print(f'Total number of rows = {len(df)}')
+    return df
 
-for model in MODELS:
-    print(f'Total number of rows of {model} = {(df['model'] == model).sum()}')
-for env in ENVIRONMENTS:
-    print(f'Total number of rows of {env} = {(df['environment'] == env).sum()}')
-
-def run_single(i):
+# Parallel run the experiments
+def run_single(i, df):
     set_seed(SEED)
     row = df.iloc[i]
     filename = log_folder + row['filename'].split('/')[-1]
@@ -72,92 +127,67 @@ def run_single(i):
         lambda_value = row['lambda_decay']
         exp_seed = row['seed']
         max_episode_steps, _, _ = set_model_and_environment_parameters(env, model)
-        # total_reward = objective_function(best_solution, tries = eval_tries, show=False, seed=SEED, model_name=model, environment_name=env, max_episode_steps=max_episode_steps, lambda_value=lambda_value)
-        record = x['record']
-        if env == 'Acrobot-v1':
-            threshold = 750
-        elif env == 'CartPole-v1':
-            threshold = -5000
-        elif env == 'MountainCar-v0':
-            threshold = 1100
-        elif env == 'LunarLander-v3':
-            threshold = -2000
-        total_reward = len(record) - np.searchsorted(record[::-1], threshold, side='right')
-        if record[-1] > threshold:
-            total_reward = None
+        total_reward = objective_function(best_solution, tries = eval_tries, show=False, seed=SEED, model_name=model, environment_name=env, max_episode_steps=max_episode_steps, lambda_value=lambda_value)
         df.loc[i, 'testing'] = total_reward
         print(f'{i}: {env} - {model} -  {exp_seed} - {total_reward}')
     return total_reward
 
 
-with ProcessPoolExecutor(max_workers=CORES) as executor:
-    testing = list(executor.map(run_single, range(len(df))))
+"""
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+MAIN FUNCTION
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
 
-df['iteration_achieved_fix'] = testing
+if __name__ == "__main__":
 
-# for i, row in df.iterrows():
-#     set_seed(SEED)
-#     filename = log_folder + row['filename'].split('/')[-1]
-#     # print(filename)
-#     with open(filename, 'rb') as f:
-#         x = pickle.load(f)
-#         best_solution = x['best_solution']
-#         # print(x['population_size'])
-#         model = row['model']
-#         env = row['environment']
-#         lambda_value = row['lambda_decay']
-#         exp_seed = row['seed']
-#         # lambda_value = 0.01
-#         max_episode_steps, _, _ = set_model_and_environment_parameters(env, model)
-#         total_reward = objective_function(best_solution, tries = eval_tries, show=False, seed=SEED, model_name=model, environment_name=env, max_episode_steps=max_episode_steps, lambda_value=lambda_value)
-#         df.loc[i, 'testing'] = total_reward
-#         print(f'{i}: {env} - {model} -  {exp_seed} - {total_reward}')
+    # Read the CSV files
+    df = pd.concat([pd.read_csv(f'{log_folder}/experiments_log_colab_exp{colab_file}.csv') for colab_file in range(1, 4)], ignore_index=True)
 
-    # break
-df.to_csv(new_file)
+    # Choose which experiments to test
+    df = filter_experiments(df)
 
+    # Print the number of experiments to be sure everything is as expected
+    print(f'Total number of rows = {len(df)}')
+    for model in MODELS:
+        print(f'Total number of rows of {model} = {(df['model'] == model).sum()}')
+    for env in ENVIRONMENTS:
+        print(f'Total number of rows of {env} = {(df['environment'] == env).sum()}')
+
+    # Run the testings
+    # with ProcessPoolExecutor(max_workers=CORES) as executor:
+    #     testing = list(executor.map(run_single, range(len(df))))
 
 
+    # from concurrent.futures import ProcessPoolExecutor
+    from itertools import repeat
+
+    with ProcessPoolExecutor(max_workers=CORES) as executor:
+        testing = list(executor.map(run_single, range(len(df)), repeat(df)))
+
+    # Store the new CSV
+    df['testing'] = testing
+    df.to_csv(new_file)
 
 
-    
-metric = 'testing'
+    # ORIGINAL VERSION (NOT PARALLEL)
+    # for i, row in df.iterrows():
+    #     set_seed(SEED)
+    #     filename = log_folder + row['filename'].split('/')[-1]
+    #     # print(filename)
+    #     with open(filename, 'rb') as f:
+    #         x = pickle.load(f)
+    #         best_solution = x['best_solution']
+    #         # print(x['population_size'])
+    #         model = row['model']
+    #         env = row['environment']
+    #         lambda_value = row['lambda_decay']
+    #         exp_seed = row['seed']
+    #         # lambda_value = 0.01
+    #         max_episode_steps, _, _ = set_model_and_environment_parameters(env, model)
+    #         total_reward = objective_function(best_solution, tries = eval_tries, show=False, seed=SEED, model_name=model, environment_name=env, max_episode_steps=max_episode_steps, lambda_value=lambda_value)
+    #         df.loc[i, 'testing'] = total_reward
+    #         print(f'{i}: {env} - {model} -  {exp_seed} - {total_reward}')
 
+        # break
 
-# 4. Make boxplots according to the "lambda_exp" column
-# df_filtered.boxplot(column=metric, by=["lambda_exp"], figsize=(10, 6))  
-# # Replace "some_metric" with the name of the column you want to plot values for
-
-# plt.title("Boxplot of Some Metric by lambda_exp")
-# plt.suptitle("")  # removes the automatic 'Boxplot grouped by lambda_exp' title
-# plt.xlabel("lambda_exp")
-# plt.ylabel("Testing fitness")
-# plt.grid(False)
-# plt.show()
-
-# ---- Compute means ----
-mean_table = (
-    df.groupby(["lambda_exp", "model"])[metric]  # replace with your metric
-    .mean()
-    .reset_index()
-)
-
-print("\nMean values of 'some_metric' by lambda_exp and model:")
-print(mean_table)
-
-# Plot with seaborn (colored by model)
-plt.figure(figsize=(10, 6))
-sns.boxplot(
-    data=df,
-    x="lambda_exp",
-    y=metric,      # replace with your column to plot
-    hue="model",          # different colors for models
-    palette="Set2"        # you can try "Set1", "Dark2", "Pastel1", "tab10", etc.
-)
-
-plt.title("Boxplot of Some Metric by lambda_exp and model")
-plt.xlabel("lambda_exp")
-plt.ylabel("Score")
-plt.legend(title="Model")
-plt.grid(False)
-plt.show()
